6aaa3abdeb6865599b59fabf1d2e80e8c0cb626c
SaadEla <sa.elaouani@gmail.com>
2020-11-06 22:30:44 +0100

    add shop and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
