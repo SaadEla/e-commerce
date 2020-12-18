435c9be2d0d13d84cc98d4b15622ec74c3a6a9c2
s.elaouani <s.elaouani@piman-group.fr>
2020-12-18 11:39:24 +0100

    add item to cart

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,18 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import './cart-icon.styles.scss';
 
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, cartItems}) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon"/>
-        <span className="item-count">0</span>
+        <span className="item-count">{cartItems.length}</span>
     </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
+const mapStatetoProps = ({cart}) => ({
+    cartItems: cart.cartItems
+  })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStatetoProps, mapDispatchToProps)(CartIcon);
