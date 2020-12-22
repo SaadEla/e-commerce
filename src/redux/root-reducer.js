c8a5f639c2564cb989fb1e3ef347918cafc5f463
s.elaouani <s.elaouani@piman-group.fr>
2020-12-22 11:37:51 +0100

    add redux-persist to app

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
 import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
+import { persistReducer } from 'redux-persist';
+//for the local storage u can import also the session storage
+import storage from 'redux-persist/lib/storage';
 
-
-export default combineReducers({
+const persistConfig = {
+    //u tell at each pont in the reducer u want to store everything
+    key: 'root',
+    storage,
+    // array containing all the reducers that i want to store
+    //(we didnt add user cause he is stored by firebase)
+    whitelist: ['cart']
+};
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
-})
+});
+export default persistReducer(persistConfig, rootReducer)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
+import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 //middleware to track action content s
 const middlewares =  [logger];
-const store = createStore(rootReducer,applyMiddleware(...middlewares));
+export const store = createStore(rootReducer,applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+
