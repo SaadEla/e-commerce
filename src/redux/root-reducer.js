4f1a32e4f34801db0c2deb0686d0abeef0974003
s.elaouani <s.elaouani@piman-group.fr>
2020-12-23 08:45:23 +0100

    add directory reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
+//reducers
 import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
+import directoryReducer from './directory/directory.reducer';
+
 import { persistReducer } from 'redux-persist';
 //for the local storage u can import also the session storage
 import storage from 'redux-persist/lib/storage';
+
 
 const persistConfig = {
     //u tell at each pont in the reducer u want to store everything
@@ -11,10 +15,11 @@ const persistConfig = {
     storage,
     // array containing all the reducers that i want to store
     //(we didnt add user cause he is stored by firebase)
-    whitelist: ['cart']
+    whitelist: ['cart', 'directory']
 };
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directoryReducer
 });
 export default persistReducer(persistConfig, rootReducer)
