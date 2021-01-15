b9c7b84e530a934a08e8f141fa572c65dbe42bd8
s.elaouani <s.elaouani@piman-group.fr>
2021-01-15 17:09:48 +0100

    add the bundle analyzer

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+      },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults" 
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+      new BundleAnalyzerPlugin()
+  ]
+}
