08d02bea642435a479088de47a3f77d4ed18dde4
SaadEla <sa.elaouani@gmail.com>
2021-02-03 13:37:00 +0100

    using get

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -21,7 +21,7 @@ class ShopPage extends React.Component {
     componentDidMount() {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
             updateCollections(collectionsMap);
             this.setState({ loading: false });
